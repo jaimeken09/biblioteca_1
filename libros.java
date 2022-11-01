7a8c0be3cd55a74a64f0feebf08a3f39bb7af9e3
jaime <jaimeken@yahoo.es>
2022-11-01 23:11:14 +0100

    numero autor qu incremente solo, y que tendremos mas de un autor.

---
+++ libros.java
@@ -0,0 +1,10 @@
+public class libros {
+    public static void main(String[] args){
+       +tiulo()String};
+    +isbn ()autonumerico;
+    autor()String;
+    +genero()String;
+    fecha publicacion()Sing;
+    +get libros()
+        +set libros;
+    +get libros;
