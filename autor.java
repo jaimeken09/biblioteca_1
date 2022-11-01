0145e7f748b49995a78a3c05e87fe8f334d8c1ab
jaime <jaimeken@yahoo.es>
2022-11-01 23:09:35 +0100

    numero autor qu incremente solo, y que tendremos mas de un autor.

---
+++ autor.java
@@ -0,0 +1,7 @@
+public class autor {
++nombre()String;
++apellidos()String;
+-fecha nacimento ()String;
+-localidad naimiento()String;
++numero autor()autonumeerico; }///numero autor autonumerico, hay mas de un autor//
+
