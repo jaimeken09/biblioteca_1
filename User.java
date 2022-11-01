6fcfdb5a4bb02a4c6e4f036ce8fd73997e12aa9b
jaime <jaimeken@yahoo.es>
2022-11-01 23:44:28 +0100

    numero autor qu incremente solo, y que tendremos mas de un autor.isbn qutonumeric. //user,autonumerico, mas de 1.

---
+++ User.java
@@ -0,0 +1,10 @@
+public class User {
+    nombre()String;
+    apellidos()String
+dni()String;
+tlfn()String;
+dirccion()String;
+poblacion()String;
++numero usuario()autonumeric;
++get user()String;
++set user(String);}
